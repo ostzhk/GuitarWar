c58afe990cc2d938d8118748b5fe8411911138d9
ostzhk <ostzhk@mail.ru>
2016-02-28 17:41:29 +0600

    New functional: -Sorting by raiting -Highlight a top score

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C//Tenderised.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -18,23 +18,29 @@ $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 or die('Cant connect to DB');
 
 // Retrieve the score data from MySQL
-$query = "SELECT * FROM guitarwars";
+$query = "SELECT * FROM guitarwars ORDER BY score DESC ";
 $data = mysqli_query($dbc, $query)
 or die('Cant execute query');
 
 // Loop through the array of score data, formatting it as HTML
 echo '<table>';
+$i = 0;
 while ($row = mysqli_fetch_array($data)) {
+    if($i==0){
+        echo '<tr><td colspan="2" class="topscoreheader">Наивысший рейтинг: <span class="raitingname">'
+            . $row['name'] . ' - ' . $row['score'] . '</span></td></tr>';
+    }
     // Display the score data
     echo '<tr><td class="scoreinfo">';
     echo '<span class="score">' . $row['score'] . '</span><br />';
     echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
-    echo '<strong>Date:</strong> ' . $row['date'] . '<br />';
+    echo '<strong>Date:</strong> ' . $row['date'] . '</td>';
     if (is_file(GW_UPLOADPATH . $row['screenshot']) && filesize(GW_UPLOADPATH . $row['screenshot']) > 0) {
-        echo '<img src=\'' . GW_UPLOADPATH . $row['screenshot'] . '\' alt="Подтверждено"/>' . '<hr/></td></tr>';
+        echo '<td><img src=\'' . GW_UPLOADPATH . $row['screenshot'] . '\' alt="Подтверждено"/>' . '</td></tr>';
     } else {
-        echo '<img src=\'' . GW_UPLOADPATH . 'unverified.gif\' alt="Не подтверждено"/><hr/></td></tr>';
+        echo '<td><img src=\'' . GW_UPLOADPATH . ' unverified.gif\' alt="Не подтверждено"/></td></tr>';
     }
+    $i++;
 }
 echo '</table>';
 
